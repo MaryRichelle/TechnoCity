e741cf01d3a838c3b1aff03191e8c92ae1fa23e8
Mary Richelle <mary.richal44@gmail.com>
2023-09-05 20:59:14 +0300

    securing Api key and url

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState, useEffect } from 'react'
 import axios from 'axios';
-
+const apiKey = process.env.REACT_APP_API_KEY;
+const URL = process.env.REACT_APP_API_URL
 export const CartContext = createContext()
 function Context({ children }) {
   const [products, setProducts] = useState([]);
@@ -11,9 +12,9 @@ function Context({ children }) {
     async function getData() {
       const options = {
         method: 'GET',
-        url: 'https://cartify.p.rapidapi.com/products',
+        url: URL,
         headers: {
-          'X-RapidAPI-Key': 'c61dd80548msh3599a386ceb3385p1550c9jsne493f06a6d3a',
+          'X-RapidAPI-Key': apiKey,
           'X-RapidAPI-Host': 'cartify.p.rapidapi.com'
         }
       };
